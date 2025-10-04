74e819cb1dbd43759beef5051376adc2790b117a
L1S4 <licha@gmail.com>
2025-10-04 00:48:11 -0300

    Cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import Header from './componentes/Header';
 import CardGame from './Juegos/CardGame';
-
+import { BrowserRouter, Routes, Route, Link, useNavigate} from 'react-router-dom';
 export default function App(){
+  
+
+  function Home() {
+    return(
+      <div>
+        <h2>Bienvenido a FunCards 🎴</h2>
+      {/* Botón que lleva al juego de cartas */}
+      <Link to="/cardgame">
+        <button className="btn">Jugar al Memory</button>
+      </Link>
+      </div>
+    )
+  }
+  
+  
+  
   return (
     <div className="app">
       <Header title="FunCards" />
       <main className="container">
         <h2>Juegos</h2>
-        {/* Por ahora mostramos el CardGame directamente */}
-        <CardGame />
+        <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cardgame" element={<CardGame />} />
+        </Routes>
+        </BrowserRouter>
+        
       </main>
     </div>
   );
--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-
+import { BrowserRouter, Routes, Route, Link, Navigate} from 'react-router-dom';
 export default function Header({ title }){
+
+  
   return (
     <header className="site-header">
       <div className="brand">{title}</div>
