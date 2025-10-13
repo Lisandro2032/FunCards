ea934f203b2f5aaa0b337fec99178a5297034a7f
L1S4 <licha@gmail.com>
2025-10-13 10:40:47 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,17 @@ import React from 'react';
 import Header from './componentes/Header';
 import CardGame from './Juegos/CardGame';
 import TicTacToe from './Juegos/CardGame/TicTacToe/Index';
-import { Routes, Route, Link } from 'react-router-dom'; // 👈 OJO: ya no importamos BrowserRouter aquí
+import { Routes, Route, Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div>
-      <h2>Bienvenido a FunCards 🎴</h2>
-      <h2>Juegos</h2>
-      {/* Botón que lleva al juego de cartas */}
+      <h2>Bienvenido a FunCards 🎴</h2>   
       <Link to="/cardgame">
-        <button className="btn">Jugar al Memory</button>
+        <button className="btnhome">Jugar al Memory</button>
       </Link>
       <Link to="/TicTacToe">
-        <button className="btn">Jugar al TicTacToe</button>
+        <button className="btnhome">Jugar al TicTacToe</button>
       </Link>
     </div>
   );
