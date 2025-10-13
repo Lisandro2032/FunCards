1283f5fb1a76dd9aac9ffb3e3e3d472626076549
L1S4 <licha@gmail.com>
2025-10-13 09:20:47 -0300

    fix: corrected CSS import case sensitivity for Netlify

--- src/Juegos/CardGame/TicTacToe/Index.jsx
+++ src/Juegos/CardGame/TicTacToe/Index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./TictacToe.css";
+import "./TicTacToe.css";
 const LINES = [
   [0,1,2],[3,4,5],[6,7,8], // filas
   [0,3,6],[1,4,7],[2,5,8], // columnas
